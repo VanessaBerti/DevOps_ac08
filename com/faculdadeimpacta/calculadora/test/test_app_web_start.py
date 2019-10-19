8ac95410d17bb795004dfa5f06c7e4a5a87ca9dd
Mayara Soares Carlini <1900958@SALAS.AULAS>
2019-10-18 21:10:50 -0300

    test

--- com/faculdadeimpacta/calculadora/test/test_app_web_start.py
+++ com/faculdadeimpacta/calculadora/test/test_app_web_start.py
@@ -7,4 +7,4 @@ def test_root_status():
 def test_root_url():
    instancia_app = app_web_start.app.test_client()
    response = instancia_app.get('/')
-   assert response.data.decode('utf-8') == 'Index Page', 'Deveria ser index page'
+   assert response.data.decode('utf-8') == 'Index Page!', 'Deveria ser index page'
